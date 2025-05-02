e03bf4983b606d42df14237a113b136dc1464e19
miguelitoelreal <miguegara20@gmail.com>
2025-05-01 20:40:41 -0500

    Inicializando

---
+++ SistemaAdopcion/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SistemaAdopcion.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
