66de1bbf7a0ba8e095f20a8c4e47e68a6e385c87
miguelitoelreal <miguegara20@gmail.com>
2025-05-02 08:27:34 -0500

    Corregir la conexion a la base de datos

--- SistemaAdopcion/Program.cs
+++ SistemaAdopcion/Program.cs
@@ -3,12 +3,14 @@ using SistemaAdopcion.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Obtener la cadena de conexión desde las variables de entorno
+var connectionString = Environment.GetEnvironmentVariable("DefaultConnection") ?? builder.Configuration.GetConnectionString("DefaultConnection");
+
 builder.Services.AddControllersWithViews();
 
 // Configurar ApplicationDbContext con PostgreSQL
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(connectionString));
 
 var app = builder.Build();
 
